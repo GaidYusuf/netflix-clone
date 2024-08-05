dc55cd4fcd36c103a74da25d83b1f5daf33572f5
Gaid Yusuf <gxy877@student.bham.ac.uk>
2024-08-05 03:45:30 +0100

    Updated sign up function in views.py file

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,7 +14,41 @@ def login(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    # retrieve form data from the POST request
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if both passwords match
+        if password == password2:
+            # Check if the email already exists in the database
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exists.")
+                return redirect('signup')  # Redirect to signup page to correct the input
+            # Check if the username already exists in the database
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists.')
+                return redirect('signup')
+            else:
+                # Create a new user if email and username are unique
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save() # Save the user to the database
+
+                # Authenticate the newly created user
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                # Log the user in
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            # If passwords do not match, show an error message
+            messages.info(request, "Passwords do not match.")
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
 
 
 def add_to_list():
